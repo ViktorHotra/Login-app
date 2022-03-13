745a9e4d4e09d800b14d6afd2d21dcc4b952e113
Viktor Hotra <zorge.mail@gmail.com>
2022-03-13 21:32:27 +0200

    Add login

---
+++ src/pages/Login.js
@@ -0,0 +1,9 @@
+export const Login = () => {
+    return <>
+        <form action="">
+            <input type="text" name='login' placeholder="login"/>
+            <input type="text" name='password' placeholder="password"/>
+            <input type="submit"/>
+        </form>
+    </>
+}
